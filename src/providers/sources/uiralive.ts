be097ed746c60b7848942e7fda484fbd7f43c2ed
samj <github@samj.app>
2025-03-01 20:25:58 +0000

    Update uiralive.ts

--- src/providers/sources/uiralive.ts
+++ src/providers/sources/uiralive.ts
@@ -1,5 +1,5 @@
 import { flags } from '@/entrypoint/utils/targets';
-import { SourcererEmbed, SourcererOutput, makeSourcerer } from '@/providers/base';
+import { SourcererOutput, makeSourcerer } from '@/providers/base';
 import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
 
@@ -7,33 +7,44 @@ import { NotFoundError } from '@/utils/errors';
 const baseUrl = 'https://xj4h5qk3tf7v2mlr9s.uira.live/';
 
 async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
-  const result = await ctx.fetcher(
-    `${baseUrl}all/${ctx.media.tmdbId}${
-      ctx.media.type === 'movie' ? '' : `?s=${ctx.media.season.number}&e=${ctx.media.episode.number}`
-    }`,
-  );
+  const fetchUrl = `${baseUrl}all/${ctx.media.tmdbId}${
+    ctx.media.type === 'movie' ? '' : `?s=${ctx.media.season.number}&e=${ctx.media.episode.number}`
+  }`;
 
-  if (!result?.sources?.length) {
-    throw new NotFoundError('No sources found');
+  let result = await ctx.fetcher(fetchUrl);
+
+  if (!result?.sources || result.sources.length === 0) {
+    try {
+      result = await ctx.fetcher(fetchUrl);
+    } catch (e: any) {
+      if (e instanceof NotFoundError) throw new NotFoundError(e.message);
+      throw e;
+    }
   }
 
-  const embeds: SourcererEmbed[] = result.sources.map((source: { source: any; url: any }) => ({
-    embedId: `uira-${source.source}`,
-    url: source.url,
-  }));
+  if (!result?.sources || result.sources.length === 0) throw new NotFoundError('No sources found');
 
   ctx.progress(90);
 
   return {
-    embeds,
+    embeds: [],
+    stream: [
+      {
+        id: 'primary',
+        playlist: result.sources[0].url,
+        type: 'hls',
+        flags: [flags.CORS_ALLOWED],
+        captions: [],
+      },
+    ],
   };
 }
 
 export const uiraliveScraper = makeSourcerer({
   id: 'uiralive',
   name: 'uira.live',
-  rank: 155,
-  disabled: true,
+  rank: 921,
+  disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
