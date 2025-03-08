eff866fa56d72d870e098d1534e68a34b980a2fb
samj <github@samj.app>
2025-03-08 18:06:00 +0000

    Update uiralive.ts

--- src/providers/sources/uiralive.ts
+++ src/providers/sources/uiralive.ts
@@ -44,7 +44,7 @@ export const uiraliveScraper = makeSourcerer({
   id: 'uiralive',
   name: 'uira',
   rank: 921,
-  disabled: true, // hopefully whitelisted soon
+  disabled: false, // im whitelisted now yipee
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
